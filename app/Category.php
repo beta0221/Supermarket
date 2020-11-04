cdbdb9674e222230c785ab47a5a49776e634387e
beta0221 <beta0221@gmail.com>
2020-11-05 01:42:43 +0800

    static method getNested Category List

--- app/Category.php
+++ app/Category.php
@@ -26,6 +26,24 @@ class Category extends Model
         return $this->belongsToMany('App\Product','category_products','category_id','product_id');
     }
 
-    
+    /**取得巢狀階層的 Category List */
+    public static function getNestedCategoryList(){
+        $categoryList = Category::orderBy('parent_id','asc')->get();
+        $indexDict=[];
+        $list = [];
+        foreach ($categoryList as $category) {
+            if(is_null($category->parent_id)){
+                $indexDict[$category->id] = count($list);
+                $list[] = $category;
+                continue;
+            }
+            $parentIndex = $indexDict[$category->parent_id];
+            if(!isset($list[$parentIndex]->subCategoryList)){
+                $list[$parentIndex]->subCategoryList = [];
+            }
+            $list[$parentIndex]->subCategoryList = array_merge($list[$parentIndex]->subCategoryList,[$category]);
+        }
+        return $list;
+    }    
 
 }
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -22,13 +22,18 @@
             </ul>
         </div>
         <div class="header__top__right__auth">
-            <a href="#"><i class="fa fa-user"></i> Login</a>
+            @auth
+            <a href="/home"><i class="fa fa-user"></i>{{auth()->user()->name}}</a>
+            @endauth
+            @guest
+            <a href="/login"><i class="fa fa-user"></i> Login</a>    
+            @endguest
         </div>
     </div>
     <nav class="humberger__menu__nav mobile-menu">
         <ul>
-            <li class="active"><a href="./index.html">Home</a></li>
-            <li><a href="./shop-grid.html">Shop</a></li>
+            <li class="{{(Request::is('/'))?'active':''}}"><a href="/">Home</a></li>
+            <li class="{{(Request::is('shop'))?'active':''}}"><a href="/shop">Shop</a></li>
             <li><a href="#">Pages</a>
                 <ul class="header__menu__dropdown">
                     <li><a href="./shop-details.html">Shop Details</a></li>
@@ -88,7 +93,12 @@
                             </ul>
                         </div>
                         <div class="header__top__right__auth">
-                            <a href="#"><i class="fa fa-user"></i> Login</a>
+                            @auth
+                            <a href="/home"><i class="fa fa-user"></i>{{auth()->user()->name}}</a>
+                            @endauth
+                            @guest
+                            <a href="/login"><i class="fa fa-user"></i> Login</a>    
+                            @endguest
                         </div>
                     </div>
                 </div>
@@ -105,16 +115,9 @@
             <div class="col-lg-6">
                 <nav class="header__menu">
                     <ul>
-                        <li class="active"><a href="./index.html">Home</a></li>
-                        <li><a href="./shop-grid.html">Shop</a></li>
-                        <li><a href="#">Pages</a>
-                            <ul class="header__menu__dropdown">
-                                <li><a href="./shop-details.html">Shop Details</a></li>
-                                <li><a href="./shoping-cart.html">Shoping Cart</a></li>
-                                <li><a href="./checkout.html">Check Out</a></li>
-                                <li><a href="./blog-details.html">Blog Details</a></li>
-                            </ul>
-                        </li>
+                        <li class="{{(Request::is('/'))?'active':''}}"><a href="/">Home</a></li>
+                        <li class="{{(Request::is('shop'))?'active':''}}"><a href="/shop">Shop</a></li>
+                        <li><a href="#">Pages</a></li>
                         <li><a href="./blog.html">Blog</a></li>
                         <li><a href="./contact.html">Contact</a></li>
                     </ul>
