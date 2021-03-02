75fcc40e0eb0256d3fbfd5e5eaa2b132ee199772
beta0221 <beta0221@gmail.com>
2021-03-02 23:41:29 +0800

    訂單內容頁面資料補齊

--- resources/views/pages/orderDetail.blade.php
+++ resources/views/pages/orderDetail.blade.php
@@ -6,41 +6,69 @@
         @include('components.breadcrumb')
     
         
-        <div class="container">
+        <div class="container mt-4 mb-4">
+
             <div class="row">
-                <table class="table table-hover" style="margin: 2rem">
-                    <thead>
-                        <tr>
-                            <th style="border: 0px"><h4>訂單編號 : {{$OR->order_numero}}<h4></th>
-                        </tr>
-                        <tr>
-                            <th>訂購商品</th>
-                            <th>單項價格</th>
-                            <th>數量</th>
-                            <th>價格</th>
-                        </tr>                      
-                    </thead>       
-                    @foreach ($OR->productList as $op)
-                        <tr>  
-                            <td>
-                            <img style="width: 70px" src="{{$op->imageUrl}}" alt="">
-                               <span class="ml-4">{{$op->name}}</span></td>
-                                <td class="align-middle">{{$op->price}}</td>
-                                <td class="align-middle">{{$op->quantity}}</td>     
-                                <td class="align-middle">{{$op->price * $op->quantity}}</td>
-                            </tr>            
-                    @endforeach       
-                        <tfoot>
+                <div class="col-md-12 mt-2 mb-2">
+                    <h4>訂單編號 : {{$OR->order_numero}}</h4>
+                </div>
+            </div>
+            <hr class="m-0">
+            
+            @if ($OR->address)
+            <div class="row">
+                <div class="col-md-12 mt-2 mb-2">
+                    <h4>訂購資訊</h4>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    收件人：{{$OR->address->name}}
+                </div>
+                <div class="col-md-12">
+                    電話：{{$OR->address->phone}}
+                </div>
+                <div class="col-md-12">
+                    地址：{{$OR->address->county}}{{$OR->address->city}}{{$OR->address->address1}}
+                </div>
+            </div>
+            @endif
+
+
+            <div class="row">
+                <div class="col-md-12 mt-2 mb-2">
+
+                    <table class="table table-hover">
+                        <thead>
                             <tr>
-                                <th colspan="4" style="
-                                font-size:1.2rem;
-                                padding-right: 6rem;
-                                text-align: end;">
-                                    總價 :<span class="text-success" style="margin-left: 1.5rem"> {{$OR->total}} </span>
-                                </th>
-                            </tr>
-                        </tfoot>
-                </table>
+                                <th>訂購商品</th>
+                                <th>單項價格</th>
+                                <th>數量</th>
+                                <th>價格</th>
+                            </tr>                      
+                        </thead>       
+                        @foreach ($OR->productList as $op)
+                            <tr>  
+                                <td>
+                                <img style="width: 70px" src="{{$op->imageUrl}}" alt="">
+                                   <span class="ml-4">{{$op->name}}</span></td>
+                                    <td class="align-middle">{{$op->price}}</td>
+                                    <td class="align-middle">{{$op->quantity}}</td>     
+                                    <td class="align-middle">{{$op->price * $op->quantity}}</td>
+                                </tr>            
+                        @endforeach       
+                    </table>
+
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-12 col-md-3 offset-md-9">
+                    {{-- <h4>小記：<span>${{$OR->total}}</span></h4> --}}
+                    <h4 class="mb-1">運費：<span>${{$OR->total_shipping}}</span></h4>
+                    <h4 class="mb-1">折扣：<span>${{$OR->total_discount}}</span></h4>
+                    <h4 class="mb-1">總額：<span class="text-success">${{$OR->total}}</span></h4>
+                </div>
             </div>
         </div>
 
